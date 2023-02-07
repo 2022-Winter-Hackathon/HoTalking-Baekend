5c3943551b49056d88fad345f15e436b7dfd67e4
pea06 <101560095+pea06@users.noreply.github.com>
2023-02-07 09:15:20 +0900

    init

---
+++ src/main/java/com/hypeboy/HoTalking/HoTalkingApplication.java
@@ -0,0 +1,13 @@
+package com.hypeboy.HoTalking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoTalkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoTalkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hypeboy/HoTalking/HoTalkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypeboy.HoTalking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoTalkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
